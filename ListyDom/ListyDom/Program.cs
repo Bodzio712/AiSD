d44790434c73f02aeeeaad4cbca932105289554d
Murzyn <bodzio_chomik@wp.pl>
2018-10-20 19:47:02 +0200

    Gotowe

--- ListyDom/ListyDom/Program.cs
+++ ListyDom/ListyDom/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListyDom
 {
@@ -11,25 +7,57 @@ namespace ListyDom
         static void Main(string[] args)
         {
             var lis = new Lista<int>();
-            lis.Add(5);
-            lis.Add(4);
-            lis.Add(100);
+            for (int i = 0; i < 10000; i++)
+                lis.Dodaj(i);
 
-            var e0 = lis.WyszukajWartoscIt(5);
-            var e1 = lis.WyszukajRefIt(e0);
+            DateTime t0;
+            DateTime t1;
+
+            Console.WriteLine("--- Wyszukiwanie po wartości ---");
+
+            t0 = DateTime.Now;
+            for (int i = 0; i < 1000; i++)
+                lis.WyszukajWartoscIt(9999);
+            t1 = DateTime.Now;
+            Console.WriteLine("Czas wyszukiwania iteracyjnego po wartości: " + (t1 - t0).TotalMilliseconds);
+
+            t0 = DateTime.Now;
+            for (int i = 0; i < 1000; i++)
+                lis.WyszukajWartoscRek(9999);
+            t1 = DateTime.Now;
+            Console.WriteLine("Czas wyszukiwania rekurencyjnego po wartości: " + (t1 - t0).TotalMilliseconds);
+
+
+            Console.WriteLine("--- Wyszukiwanie po referencji ---");
+
+            t0 = DateTime.Now;
+            for (int i = 0; i < 1000; i++)
+                lis.WyszukajRefIt(lis.GetOgon());
+            t1 = DateTime.Now;
+            Console.WriteLine("Czas wyszukiwania iteracyjnego po referencji: " + (t1 - t0).TotalMilliseconds);
+
+            t0 = DateTime.Now;
+            for (int i = 0; i < 1000; i++)
+                lis.WyszukajRefRek(lis.GetOgon());
+            t1 = DateTime.Now;
+            Console.WriteLine("Czas wyszukiwania rekurencyjnego po referencji: " + (t1 - t0).TotalMilliseconds);
+
         }
     }
 
     class Lista<T>
     {
         private Element<T> _glowka { get; set; }
+        private Element<T> _ogon { get; set; }
+
 
         public Lista()
         {
             _glowka = null;
+            _ogon = null;
         }
 
-        public void Add(T wartosc)
+        public void Dodaj(T wartosc)
         {
             if (_glowka == null)
             {
@@ -37,28 +65,51 @@ namespace ListyDom
                 _glowka = e;
                 e.Poprzedni = e;
                 e.Nastepny = null;
-                e.Ostatni = e;
+                _ogon = e;
             }
             else
             {
                 var e = new Element<T>(wartosc);
-                e.Poprzedni = _glowka.Ostatni;
+                e.Poprzedni = _ogon;
                 e.Nastepny = null;
-                e.Ostatni = e;
-                _glowka.Nastepny = e;
-                _glowka.Ostatni = e;
+                _ogon = e;
+                e.Poprzedni.Nastepny = e;
             }
         }
 
-        public void Delete (Element<T> e)
+        public void Usun(Element<T> e)
         {
+            e.Poprzedni.Nastepny = e.Nastepny;
+            if (e.Nastepny == null)
+                _glowka = e.Poprzedni;
+            else
+            {
+                _ogon = e.Nastepny;
+                e.Nastepny.Poprzedni = e.Poprzedni;
+            }
+        }
 
+        public void WstawPoElemencie(T wartosc, Element<T> element)
+        {
+            var elementPoKtorym = WyszukajRefIt(element);
+            var wstawiany = new Element<T>(wartosc);
+            if (elementPoKtorym.Nastepny == null)
+            {
+                elementPoKtorym.Nastepny = wstawiany;
+                wstawiany.Nastepny = null;
+                _ogon = wstawiany;
+            }
+            else
+            {
+                wstawiany.Nastepny = elementPoKtorym.Nastepny;
+                elementPoKtorym.Nastepny = wstawiany;
+            }
         }
 
         public Element<T> WyszukajWartoscIt(T wart)
         {
             Element<T> x = null;
-            Element<T> ob = this._glowka;
+            Element<T> ob = _glowka;
 
             while (true)
             {
@@ -99,6 +150,60 @@ namespace ListyDom
 
             return x;
         }
+
+
+        public Element<T> WyszukajRefRek(Element<T> referencja)
+        {
+            Element<T> element = _glowka;
+            if (element.Equals(referencja))
+                return referencja;
+            else if (element.Nastepny == null)
+                return null;
+            else
+                return WyszukajRefRek(referencja, element.Nastepny);
+        }
+
+        private Element<T> WyszukajRefRek(Element<T> referencja, Element<T> element)
+        {
+            if (element.Equals(referencja))
+                return referencja;
+            else if (element.Nastepny == null)
+                return null;
+            else
+                return WyszukajRefRek(referencja, element.Nastepny);
+        }
+
+        public Element<T> WyszukajWartoscRek(T wart)
+        {
+            Element<T> element = _glowka;
+            if (element.Wartosc.Equals(wart))
+                return element;
+            else if (element.Nastepny == null)
+                return null;
+            else
+                return WyszukajWartoscRek(wart, element.Nastepny);
+        }
+
+        public Element<T> WyszukajWartoscRek(T wart, Element<T> element)
+        {
+            if (element.Wartosc.Equals(wart))
+                return element;
+            else if (element.Nastepny == null)
+                return null;
+            else
+                return WyszukajWartoscRek(wart, element.Nastepny);
+        }
+
+        public Element<T> GetHead()
+        {
+            return _glowka;
+        }
+
+        public Element<T> GetOgon()
+        {
+            return _ogon;
+        }
+
     }
 
     class Element<T>
@@ -106,19 +211,17 @@ namespace ListyDom
         public T Wartosc { get; set; }
         public Element<T> Nastepny { get; set; }
         public Element<T> Poprzedni { get; set; }
-        public Element<T> Ostatni { get; set; }
 
         public Element(T wartosc)
         {
             Wartosc = wartosc;
         }
 
-        public Element(T wartosc, Element<T> nastepny, Element<T> poprzedni, Element<T> ostatni)
+        public Element(T wartosc, Element<T> nastepny, Element<T> poprzedni)
         {
             Wartosc = wartosc;
             Nastepny = nastepny;
             Poprzedni = poprzedni;
-            Ostatni = ostatni;
         }
     }
 }
